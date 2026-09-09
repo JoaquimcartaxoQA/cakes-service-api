9eec542b21b4681cf4d73a581ad622d432af112a
Joaquim Ayres Cartaxo Brasil <jcartaxo.bjj@gmail.com>
2026-09-09 14:43:08 -0300

    Adiciona senha 7Bytes

--- src/main/java/io/cip/services/cake/config/SecurityConfig.java
+++ src/main/java/io/cip/services/cake/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             AuthenticationManagerBuilder auth,
             PasswordEncoder passwordEncoder,
             @Value("${cakes.authentication.username}") String userName,
-            @Value("${cakes.authentication.password}") String password
+            @Value("cake123") String password
     ) throws Exception {
         auth
                 .inMemoryAuthentication()
